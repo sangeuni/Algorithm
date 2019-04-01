d04d5fe4b852ce41d85545ac9e07c8c45f0c5091
Sangeun Lee <goakgoak1995@gmail.com>
2019-04-01 14:06:10 +0900

    boj 2750

---
+++ src/main/java/_2750.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class _2750 {
+    // 수 정렬하기
+
+    public static void main(String[] args) throws IOException {
+        _2750 main = new _2750();
+        main.go();
+    }
+
+    private void go() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            arr[index++] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            bw.write(Integer.toString(arr[i]) + "\n");
+
+        }
+        bw.flush();
+        bw.close();
+    }
+}
