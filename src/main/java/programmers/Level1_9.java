be540a4b801387a0e509e440880f14e5d4b0b41c
Sangeun Lee <goakgoak1995@gmail.com>
2019-07-09 02:57:01 +0900

    Level1 - 문자열 다루기 기본

---
+++ src/main/java/programmers/Level1_9.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Level1_9 {
+    /*
+     * Level1 - 문자열 다루기 기본
+     * */
+    public boolean solution(String s) {
+        boolean answer = false;
+        if (s.length() == 4 || s.length() == 6) {
+
+            for (int i = 0; i < s.length(); i++) {
+                if (!Character.isDigit(s.charAt(i))) {
+                    answer = false;
+                    break;
+                } else
+                    answer = true;
+            }
+        }
+        return answer;
+    }
+}
