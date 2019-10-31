5860d5fb0eab4bc24706c9fb440775e67288ed1a
Sangeun Lee <goakgoak1995@gmail.com>
2019-10-31 17:30:01 +0900

    boj - 1149

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 public class Main {
-	
+	private void go() {
+	}
+
 	public static void main(String[] args) {
 		Main main = new Main();
 		main.go();
-
-	}
-
-	private void go() {
 
 	}
 }
---
+++ src/main/java/boj/_1149.java
@@ -0,0 +1,33 @@
+package boj;
+
+import java.util.Scanner;
+
+public class _1149 {
+	int n;
+	int r, g, b;
+	int temp_r, temp_g, temp_b;
+
+	private void go() {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		r = sc.nextInt();
+		g = sc.nextInt();
+		b = sc.nextInt();
+
+		for (int i = 1; i < n; i++) {
+			temp_r = sc.nextInt();
+			temp_g = sc.nextInt();
+			temp_b = sc.nextInt();
+
+			temp_r += Math.min(g, b);
+			temp_g += Math.min(r, b);
+			temp_b += Math.min(r, g);
+
+			r = temp_r;
+			g = temp_g;
+			b = temp_b;
+		}
+
+		System.out.println(Math.min(Math.min(r, g), b));
+	}
+}
