9895945bc8f7cfe0ab9a8c3a0694dc7efa5f9e35
Sangeun Lee <goakgoak1995@gmail.com>
2020-01-03 14:38:54 +0900

    boj - 1978

--- java/boj/_1978.java
+++ java/boj/_1978.java
@@ -1,11 +1,17 @@
+package boj;
+
 import java.util.Scanner;
 
-public class Main {
+public class _1978 {
+	/* 소수 찾기
+	 * 2 <= i <= n-1
+	 * 2 <= i <= n/2
+	 * 2 <= i*i <= n (2 <= i <= root n)
+	 */
 	public void go() {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int count = 0;
-
 		for (int i = 0; i < size; i++) {
 			int n = sc.nextInt();
 			if (isPrime(n))
@@ -17,15 +23,10 @@ public class Main {
 	public boolean isPrime(int n) {
 		if (n < 2)
 			return false;
-		for (int j = 2; j <= n - 1; j++) {
-			if (n % j == 0)
+		for (int i = 2; i*i <= n; i++) {
+			if (n % i == 0)
 				return false;
 		}
 		return true;
-	}
-
-	public static void main(String[] args) {
-		Main main = new Main();
-		main.go();
 	}
 }
