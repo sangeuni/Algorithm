8055e6a0e53aeda4fb72422bc192ab16b9f4c0ef
Sangeun Lee <goakgoak1995@gmail.com>
2020-04-28 21:09:57 +0900

    Update _17142.java

--- java/boj/_17142.java
+++ java/boj/_17142.java
@@ -1,143 +1,154 @@
 package boj;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class _17142 {
-	/* 
+	/*
 	 * 연구소 3
 	 */
 	static int n, m, min;
+	static int[][] map, copy, visited;
 	static int[] a;
+	static boolean[] c;
+	static List<Dot> virusList;
 	static int[] dx = { 1, -1, 0, 0 };
 	static int[] dy = { 0, 0, 1, -1 };
-	static int[][] map, copy, visited;
-	static List<Node> virus;
-	static Queue<Node> q;
+	static StringTokenizer st;
+	static Deque<Dot> q;
 
-	static class Node {
-		int x, y;
+	static class Dot {
+		private int x, y, step;
 
-		public Node(int x, int y) {
+		public Dot(int x, int y, int step) {
 			this.x = x;
 			this.y = y;
+			this.step = step;
 		}
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringTokenizer st = new StringTokenizer(sc.nextLine());
-		n = stoi(st.nextToken());
-		m = stoi(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		input();
+		permutation(0, 0);
 
-		map = new int[n][n];
-		a = new int[m];
-		virus = new ArrayList<>();
-
-		int vCount = 0;
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(sc.nextLine());
-			for (int j = 0; j < n; j++) {
-				map[i][j] = stoi(st.nextToken());
-				if (map[i][j] == 1) {
-					map[i][j] = -2;
-				}
-				if (map[i][j] == 2) {
-					map[i][j] = -1;
-					virus.add(new Node(i, j));
-					vCount++;
-				}
-			}
-		}
-		min = Integer.MAX_VALUE;
-		permutation(0, 0, vCount);
-		if(min == Integer.MAX_VALUE)
-			min = -1;
+		min = (min == Integer.MAX_VALUE) ? -1 : min;
 		System.out.println(min);
 	}
 
-	static void permutation(int depth, int start, int size) {
-		if (depth == m) {
-			simulation();
+	static void solve() {
+		// 바이러스 놓기
+		for (int i : a) {
+			Dot virus = virusList.get(i);
+			copy[virus.x][virus.y] = 0;
+			q.offer(virus);
+			visited[virus.x][virus.y] = 1;
+		}
+
+		int time = bfs();
+
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (copy[i][j] == -1) {
+					count++;
+				}
+			}
+		}
+
+		if (count == 0) {
+			min = Math.min(min, time);
+		}
+	}
+
+	static int bfs() {
+		int max = 0;
+		while (!q.isEmpty()) {
+			Dot cur = q.poll();
+			max = Math.max(max, copy[cur.x][cur.y]);
+			int nx, ny;
+			for (int i = 0; i < 4; i++) {
+				nx = cur.x + dx[i];
+				ny = cur.y + dy[i];
+
+				if (nx >= 0 && ny >= 0 && nx < n && ny < n && visited[nx][ny] == 0 && copy[nx][ny] <= 0) {
+					visited[nx][ny] = 1;
+					if (copy[nx][ny] == -2) {
+						q.offer(new Dot(nx, ny, cur.step + 1));
+					}
+					if (copy[nx][ny] == -1) {
+						copy[nx][ny] = cur.step + 1;
+						q.offer(new Dot(nx, ny, cur.step + 1));
+					}
+
+				}
+			}
+		}
+//		print(copy);
+		return max;
+	}
+
+	static void permutation(int count, int start) {
+		if (count == m) {
+			ready();
+			solve();
 			return;
 		}
 
-		for (int i = start; i < size; i++) {
-			a[depth] = i;
-			permutation(depth + 1, i + 1, size);
+		for (int i = start; i < virusList.size(); i++) {
+			if (c[i])
+				continue;
+			a[count] = i;
+			c[i] = true;
+			permutation(count + 1, i + 1);
+			c[i] = false;
 		}
 	}
 
-	static void simulation() {
+	static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = stoi(st.nextToken());
+		m = stoi(st.nextToken());
 		init();
-		for (int i : a) {
-			q.offer(virus.get(i));
-			copy[virus.get(i).x][virus.get(i).y] = 0;
-			visited[virus.get(i).x][virus.get(i).y] = 1;
-		}
-		bfs();
-		int result = calc();
-		if(result > -1)
-			min = Math.min(min, result);
-		print(copy);
-		System.out.println(result);
-	}
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = stoi(st.nextToken());
 
-	static void bfs() {
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-
-				if (nx < 0 || ny < 0 || nx >= n || ny >= n)
-					continue;
-
-				if (copy[nx][ny] == 0 && visited[nx][ny] == 0) {
-					copy[nx][ny] = copy[cur.x][cur.y] + 1;
-					q.offer(new Node(nx, ny));
-					visited[nx][ny] = 1;
+				if (map[i][j] == 0) {
+					map[i][j] = -1;
 				}
-				if (copy[nx][ny] == -1 && visited[nx][ny] == 0) {
-					copy[nx][ny] = copy[cur.x][cur.y] + 1;
-					q.offer(new Node(nx, ny));
-					visited[nx][ny] = 1;
+				if (map[i][j] == 2) {
+					map[i][j] = -2;
+					virusList.add(new Dot(i, j, 0));
 				}
 			}
 		}
+		c = new boolean[virusList.size()];
 	}
 
-	static int calc() {
-		int result = -1;
-		int count = 0;
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<n; j++) {
-				if(copy[i][j] < 0)
-					continue;
-				result = Math.max(result, copy[i][j]);
-				if(copy[i][j] == 0 && visited[i][j] == 0) count++;
-			}
-		}
-		
-		if(count > 0) return -1;
-		return result;
-	}
-	
-	static void init() {
-		visited = new int[n][n];
-		copy = new int[n][n];
+	static void ready() {
 		q = new LinkedList<>();
-
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<n;j++) {
+		copy = new int[n][n];
+		visited = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
 				copy[i][j] = map[i][j];
 			}
 		}
-		
+	}
+
+	static void init() {
+		map = new int[n][n];
+		a = new int[m];
+
+		virusList = new LinkedList<>();
+		min = Integer.MAX_VALUE;
 	}
 
 	static int stoi(String s) {
@@ -145,18 +156,12 @@ public class _17142 {
 	}
 
 	static void print(int[][] a) {
-		for (int i = 0; i < n; i++) {
-			for(int j = 0; j<n; j++) {
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length; j++) {
 				System.out.print(a[i][j] + " ");
 			}
 			System.out.println();
 		}
-		System.out.println();
-	}
-	static void print() {
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
-		System.out.println();
+		System.out.println("------------------");
 	}
 }
