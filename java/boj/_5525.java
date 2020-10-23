a34a2856c7bab9bc6a1625e60add561627bc41f1
Sangeun Lee <goakgoak1995@gmail.com>
2020-10-23 14:08:44 +0900

    edit boj - 5525

--- java/boj/_5525.java
+++ java/boj/_5525.java
@@ -11,25 +11,56 @@ public class _5525 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = stoi(br.readLine());
-        len = stoi(br.readLine());
-        s = br.readLine();
+        int n = stoi(br.readLine());
+        int m = stoi(br.readLine());
+        char[] s = br.readLine().toCharArray();
 
-        int ioi = 0;
-        int answer = 0;
-        for (int i = 1; i < len - 1; i++) {
-            if (s.charAt(i - 1) == 'I' && s.charAt(i) == 'O' && s.charAt(i + 1) == 'I') {
-                ioi++;
-                if (ioi == n) {
-                    ioi--;
-                    answer++;
+        StringBuilder sb = new StringBuilder("I");
+        for (int i = 0; i < n; i++) {
+            sb.append("OI");
+        }
+
+        char[] p = sb.toString().toCharArray();
+
+        System.out.println(kmp(s, p));
+    }
+
+    private static int kmp(char[] s, char[] p) {
+        int count = 0;
+        int n = s.length;
+        int m = p.length;
+        int j = 0;
+        int[] pi = getPi(p);
+
+        for (int i = 0; i < n; i++) {
+            while (j > 0 && s[i] != p[j]) {
+                j = pi[j - 1];
+            }
+            if (s[i] == p[j]) {
+                if (j == m - 1) {
+                    count++;
+                    j = pi[j];
+                } else {
+                    j++;
                 }
-                i++;
-            } else {
-                ioi = 0;
             }
         }
-        System.out.println(answer);
+        return count;
+    }
+
+    private static int[] getPi(char[] p) {
+        int m = p.length;
+        int[] pi = new int[m];
+        int j = 0;
+        for (int i = 1; i < m; i++) {
+            while (j > 0 && p[i] != p[j]) {
+                j = pi[j - 1];
+            }
+            if (p[i] == p[j]) {
+                pi[i] = ++j;
+            }
+        }
+        return pi;
     }
 
     private static int stoi(String s) {
