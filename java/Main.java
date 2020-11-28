cea8f4729225d2f45e6c850f9c54d849b847b0cb
Sangeun Lee <goakgoak1995@gmail.com>
2020-11-28 17:21:52 +0900

    boj - 2887

--- java/Main.java
+++ java/Main.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
